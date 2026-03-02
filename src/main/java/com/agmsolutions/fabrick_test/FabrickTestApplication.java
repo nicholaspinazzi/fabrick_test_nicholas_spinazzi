912d3a185556e47ba2d055953c50fbe627a96d3a
nicholas spinazzi <nicholas.spinazzi@agmsolutions.net>
2026-03-02 11:29:58 +0100

    first commit

---
+++ src/main/java/com/agmsolutions/fabrick_test/FabrickTestApplication.java
@@ -0,0 +1,13 @@
+package com.agmsolutions.fabrick_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabrickTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agmsolutions/fabrick_test/FabrickTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agmsolutions.fabrick_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
